5b84f649ad6fe829ee8f7f1b9485cc09ba69c51f
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-02-08 16:42:43 +0530

    shit continuous

--- env-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'process.env.NEXT_PUBLIC_SERVICE_ID': process.env.NEXT_PUBLIC_SERVICE_ID,
-  'process.env.NEXT_PUBLIC_TEMPLATE_ID': process.env.NEXT_PUBLIC_TEMPLATE_ID,
-  'process.env.NEXT_PUBLIC_USER_ID': process.env.NEXT_PUBLIC_USER_ID,
-}
