5506193ac3a85eb02e9b9e11a0a58e5df03a9eec
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-02-08 15:44:55 +0530

    updated the env package

---
+++ env-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'process.env.NEXT_PUBLIC_SERVICE_ID': process.env.NEXT_PUBLIC_SERVICE_ID,
+  'process.env.NEXT_PUBLIC_TEMPLATE_ID': process.env.NEXT_PUBLIC_TEMPLATE_ID,
+  'process.env.NEXT_PUBLIC_USER_ID': process.env.NEXT_PUBLIC_USER_ID,
+}
