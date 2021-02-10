2e71ceeac3ef857a442d377da63beb1fcb530c2f
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-02-10 14:23:05 +0000

    Updates

---
+++ _next/static/-YyPogK870dXGleyyvJKL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-33d42779ed31c5d380aa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4121203d2c15c2197b12.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/97iPsmcL8uaMAHV6ZxrxT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6e2bcfee8c0d06ca5726.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4121203d2c15c2197b12.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
