d194c7cb4d17975762de2c678bad8444dae60ec4
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-05-12 15:23:40 +0530

    trying to fix analytics

--- pages/analytics.js
+++
@@ -1,5 +0,0 @@
-document.window.dataLayer = document.window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', `${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`);
