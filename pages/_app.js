91c395e9f9fb01b0df08c2c852f176251d310154
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-05-12 22:14:44 +0530

    Added posthog Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,13 @@ import '../styles/globals.css'
 import ThemeProvider from '../theme/themeProvider'
 import { CookiesProvider } from 'react-cookie'
 
+import posthog from 'posthog-js'
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    if (!window.location.href.includes('localhost')) {
+      posthog.init(`${process.env.NEXT_PUBLIC_POSTHOG_ID}`, { api_host: 'https://app.posthog.com' })
+    }
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
