4517bdc8c75e57cb120fc56b411cd21b29ec3b46
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2021-05-12 16:46:38 +0000

    Updates

--- _next/static/-4Ybi9QAcf2JOspgzU9wc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-014f2e1de18e067ffa58.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c7832ad7a622f59a0c7b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
